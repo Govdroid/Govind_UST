ed0e530a5eff931f3be936bbcedea1e9947da2d6
Govind Nandakumar <85785236+Govdroid@users.noreply.github.com>
2024-09-28 07:18:09 +0530

    Delete JobNotificationApplication directory

--- JobNotificationApplication/src/main/java/com/ust/JobNotificationApplication/repo/JobRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ust.JobNotificationApplication.repo;
-
-
-import com.ust.JobNotificationApplication.model.JobNotification;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface JobRepository extends JpaRepository<JobNotification, Long> {
-}
