a843542cee6dc5391d0fa168e497d7c6fa9a9271
Govind Nandakumar <85785236+Govdroid@users.noreply.github.com>
2024-10-04 17:44:11 +0530

    Delete Laptop_Purchase directory

--- Laptop_Purchase/src/main/resources/schema.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS product (
-    id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255),
-    category VARCHAR(255),
-    brand VARCHAR(255),
-    price DOUBLE,
-    description TEXT
-);
